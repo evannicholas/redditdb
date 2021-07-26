2fc60ef3e272177273f66d1c8afd66ad3032ce90
evannicholas <evannicholasabednego@gmail.com>
2021-07-26 12:01:25 +0700

    Semi Reddit done

---
+++ src/components/Comment.vue
@@ -0,0 +1,37 @@
+<template>
+  <div v-if="comment != undefined">
+    <div class="row comment q-ma-sm q-pa-md">
+      <div class="col-1">
+        {{ comment.ups }}
+      </div>
+      <div class="col-11">
+        <div class="row author">
+          /u/{{ comment.author }}
+        </div>
+        <div class="row">
+          {{ comment.body }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    comment: Object,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.comment {
+  background-color: $dark;
+  border-radius: 5px;
+}
+.author {
+   margin-bottom: 10px;
+   color: $primary;
+   font-weight: bold;
+}
+</style>
--- src/pages/Comments.vue
+++ src/pages/Comments.vue
@@ -1,21 +1,35 @@
 <template>
   <div>
-    {{ $route.params.url }}
+    <!-- post -->
+    <div v-for="comment in comments" :key="comment.data.permalink">
+      <CommentComponents :comment="comment.data"/>
+    </div>
   </div>
 </template>
 
 <script>
 import { api } from "boot/axios";
+import CommentComponents from "components/Comment.vue";
 export default {
+  components: {
+    CommentComponents,
+  },
+  data() {
+    return {
+      post: {},
+      comments: [],
+    };
+  },
   methods: {
     loadPermalink() {
       const modifiedPermalink = this.$route.params.url;
       const permalink = modifiedPermalink.replaceAll(".", "/");
-      console.log(`https://www.reddit.com${permalink}.json`);
-        api.get(`https://www.reddit.com${permalink}.json`).then((response) => {
-        //   this.posts = response.data.data.children;
+      console.log(`http://www.reddit.com${permalink}.json`);
+      api.get(`http://www.reddit.com${permalink}.json`).then((response) => {
+        this.post = response.data[0].data.children[0].data;
+        this.comments = response.data[1].data.children;
         console.log(response.data);
-        });
+      });
     },
   },
   mounted() {
